ad04521c2f41242503fc239b5fa5df3534ecb713
masud90895 <masudhossainmbs129@gmail.com>
2023-01-06 22:47:49 +0600

    error page added

--- src/Components/Products/Product.js
+++ src/Components/Products/Product.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 
 const Product = ({product}) => {
     const [quantity,setQuantity] = useState(1)
@@ -6,7 +7,11 @@ const Product = ({product}) => {
         setQuantity(quantity+1);
     }
     const handleQuantityDiscress=()=>{
-        
+        if(quantity === 1){
+            return toast.error("You can't add less than one quantity")
+        }
+        setQuantity(quantity-1);
+
     }
    
     return (
@@ -17,10 +22,10 @@ const Product = ({product}) => {
             
             <p className='text-xl'>Price : <span>${product.price * quantity}</span></p>
 
-            <div className='flex '>
-                <h1 onClick={handleQuantityIncress} className='text-3xl bg-gray-300 px-4 py-2 border-l-2'>+</h1>
+            <div className='flex my-2'>
+                <h1 onClick={handleQuantityIncress} className='text-3xl bg-gray-300 px-4 py-2 border-l-2 cursor-pointer'>+</h1>
                 <h1 className='text-3xl bg-gray-300 px-4 py-2 border-l-2'>{quantity}</h1>
-                <h1 className='text-3xl bg-gray-300 px-4 py-2 border-l-2'>-</h1>
+                <h1 onClick={handleQuantityDiscress} className='text-3xl bg-gray-300 px-4 py-2 border-l-2 cursor-pointer'>-</h1>
             </div>
 
             <button className='bg-purple-600  w-full text-center my-3 text-xl p-2 rounded text-white hover:bg-white border hover:border-purple-600 hover:text-black'>Add To Cart</button>
--- src/Router/Router.js
+++ src/Router/Router.js
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
+import Login from "../Components/Authentication/Login";
+import Register from "../Components/Authentication/Register";
+import ErrorPage from "../Components/ErrorPage/ErrorPage";
 import Main from "../Layout/Main";
 
 const router = createBrowserRouter([
     {
       path: "/",
-      element : <Main/>
+      element : <Main/>,
+      errorElement: <ErrorPage/>
+    },
+    {
+        path: "/login",
+        element : <Login/>
+    },
+    {
+        path : "/register",
+        element : <Register/>
     }
   ])
 
